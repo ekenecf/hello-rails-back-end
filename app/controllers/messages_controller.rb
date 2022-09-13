1d8fd948a9246812f7f19e276ddc664de0378be1
ekenecf <ekens2u@gmail.com>
2022-09-13 18:31:52 +0100

    Fix rubocop

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
-    def index
-      id = rand(1..Message.count)
-      if id.nil?
-        render json: { error: 'sorry! not found' }.to_json, status: 404
-      else
-        @message = Message.find(id)
-        render json: @message
-        puts @message.greetings
-      end
+  def index
+    id = rand(1..Message.count)
+    if id.nil?
+      render json: { error: 'sorry! not found' }.to_json, status: 404
+    else
+      @message = Message.find(id)
+      render json: @message
+      puts @message.greetings
     end
+  end
 end
